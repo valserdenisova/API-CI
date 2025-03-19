7c9973096c65f2ace82b314ed03003972d91a00b
Valeriya Mishurova <tvoyadenisova@yandex.ru>
2025-03-19 20:58:02 +0300

    Added JSON

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
@@ -15,7 +16,9 @@ class MobileBankApiTestV1 {
                 .when()
                 .get("/demo/accounts")
                 // Проверки
+                // код теста
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
